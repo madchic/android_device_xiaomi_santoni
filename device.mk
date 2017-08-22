bbab89b33b764c9e2222e0484abab3e30430c74b
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:41:25 -0700

    santoni: Add DRM HAL impl

--- device.mk
+++ device.mk
@@ -187,6 +187,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.0-service-custom

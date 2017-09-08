54e98863c077c1d0d9d4e2e0ebdde342e68ba4d1
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-09 01:22:01 +0530

    santoni: Remove sepolicies for now
    Change-Id: I6cb0a690e954cdaeefc496515d0c87d16f6bace0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -259,7 +259,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true

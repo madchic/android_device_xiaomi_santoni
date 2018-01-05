b77b5efa14764fe5bf8ba50c75244f0e7ed02954
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2018-01-05 17:58:10 -0200

    Add hosts here

--- device.mk
+++ device.mk
@@ -257,6 +257,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
+# Hosts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hosts:system/etc/hosts
+
 # Wifi
 PRODUCT_PACKAGES += \
     libqsap_sdk \
